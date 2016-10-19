90f92640ca4d6f002b982c1cd646f7b42646da6c
Kyle Lauritzen <Kyle Lauritzen>
2016-10-18 22:30:05 -0600

    current deck in Deck Builder mode is functional

--- public/resources/js/controllers/deckCtrl.js
+++ public/resources/js/controllers/deckCtrl.js
@@ -1,3 +1,49 @@
 app.controller('deckCtrl', function ($scope) {
-
+    $scope.currentDeck = [
+        {
+            name: "Archers",
+            cost: 3,
+            picture: "resources/images/ArchersCard.png"
+        },
+        {
+            name: "Baby Dragon",
+            cost: 4,
+            picture: "resources/images/BabyDragonCard.png"
+        },
+        {
+            name: "Barbarians",
+            cost: 5,
+            picture: "resources/images/BarbariansCard.png"
+        },
+        {
+            name: "Bomber",
+            cost: 3,
+            picture: "resources/images/BomberCard.png"
+        },
+        {
+            name: "Bowler",
+            cost: 5,
+            picture: "resources/images/BowlerCard.png"
+        },
+        {
+            name: "Balloon",
+            cost: 5,
+            picture: "resources/images/BalloonCard.png"
+        },
+        {
+            name: "Dark Prince",
+            cost: 4,
+            picture: "resources/images/DarkPrinceCard.png"
+        },
+        {
+            name: "Fire Spirits",
+            cost: 5,
+            picture: "resources/images/FireSpiritsCard.png"
+        }
+        
+        
+        
+        
+        
+    ];
 });
