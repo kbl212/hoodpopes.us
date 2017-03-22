28ded225c77f328cefa66fd9247be44edb2f4399
Kyle Lauritzen <kylelauritzen@US-GN-41980.local>
2017-03-22 14:22:57 -0400

    using new auth token

--- public/resources/js/controllers/cocCtrl.js
+++ public/resources/js/controllers/cocCtrl.js
@@ -3,7 +3,7 @@ app.controller('cocCtrl', function ($scope, $http) {
         $.ajax({
             url: "https://api.clashofclans.com/v1/clans/%232G88GQCV",
             beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkyOWFjODFjLTZjOTQtNDRjZS04ZDJmLWM0OWFmYTkzOTY0ZSIsImlhdCI6MTQ5MDEyNTg2OSwic3ViIjoiZGV2ZWxvcGVyL2UyNGZmY2I0LTA2MTAtYTY4Mi1mMjk0LWEyNTZlYWJmNjAwMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE2Mi4yNTUuMTE5LjI0OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.W-LcnlxLbn37J5YusEWmrpTbbBjKhnwUjwTmUUbD9MdfJqz3-pCtsoJFO6iOBZU5315E8qLIhEmqF2NJ4D0JGw");
+                xhr.setRequestHeader('Authorization', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg1MTA1M2JmLTlhNGItNDY3Zi1iNWNmLWIxN2EyYmIwOTljMiIsImlhdCI6MTQ5MDIwNjk0MCwic3ViIjoiZGV2ZWxvcGVyL2UyNGZmY2I0LTA2MTAtYTY4Mi1mMjk0LWEyNTZlYWJmNjAwMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwNy4yMC4xNTEuMjMiXSwidHlwZSI6ImNsaWVudCJ9XX0.yqX51HxICm9vAkJ6HEuYeuj62KmRzMSQqZu9czU9U_GfcSsAmorfOgYGx5xDAbRGtOULREtPML0wI8QRn4s7oQ");
             },
             type: 'GET',
             dataType: 'jsonp',
