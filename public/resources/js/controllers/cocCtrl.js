51958a6bb2c50ee8f7c820f6231cdcba1b4ca42f
Kyle Lauritzen <kylelauritzen@US-GN-41980.local>
2017-03-22 14:13:41 -0400

    changed url, added v1

--- public/resources/js/controllers/cocCtrl.js
+++ public/resources/js/controllers/cocCtrl.js
@@ -1,7 +1,7 @@
 app.controller('cocCtrl', function ($scope, $http) {
     function getClanInfo() {
         $.ajax({
-            url: "https://developer.clashofclans.com/#/clans/%232G88GQCV",
+            url: "https://api.clashofclans.com/v1/clans/%232G88GQCV",
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('Authorization', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkyOWFjODFjLTZjOTQtNDRjZS04ZDJmLWM0OWFmYTkzOTY0ZSIsImlhdCI6MTQ5MDEyNTg2OSwic3ViIjoiZGV2ZWxvcGVyL2UyNGZmY2I0LTA2MTAtYTY4Mi1mMjk0LWEyNTZlYWJmNjAwMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE2Mi4yNTUuMTE5LjI0OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.W-LcnlxLbn37J5YusEWmrpTbbBjKhnwUjwTmUUbD9MdfJqz3-pCtsoJFO6iOBZU5315E8qLIhEmqF2NJ4D0JGw");
             },
