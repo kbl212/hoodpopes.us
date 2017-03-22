15868ea446570449e37664d130f13017e621aa3c
Kyle Lauritzen <kylelauritzen@US-GN-41980.local>
2017-03-22 17:54:04 -0400

    trying quotaguardstatic

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 
+var request = require('request');
+
+var options = {
+    proxy: process.env.QUOTAGUARDSTATIC_URL,
+    url: 'http://ip.jsontest.com/',
+    headers: {
+        'User-Agent': 'node.js'
+    }
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
 app.listen(port, function () {
     console.log('connected to port ', port);
 })
