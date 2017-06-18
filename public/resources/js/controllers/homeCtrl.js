72e1c3422375c908be2e50711b0749ef63057ae4
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2017-06-18 19:42:34 -0400

    removed last api key, added new, protected one.

--- public/resources/js/controllers/homeCtrl.js
+++ public/resources/js/controllers/homeCtrl.js
@@ -1,9 +1,10 @@
 app.controller('homeCtrl', function($scope) {
+    $scope.cocApiKey = config.cocApiKey;
     function getClanInfo() {
         $.ajax({
             url: "https://api.clashofclans.com/v1/leagues",
             beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImZhZWViODgxLWI3MTktNDFmZS05NDUyLTFmYTJkMjhmYzdjMyIsImlhdCI6MTQ5MDIxODYyNiwic3ViIjoiZGV2ZWxvcGVyL2UyNGZmY2I0LTA2MTAtYTY4Mi1mMjk0LWEyNTZlYWJmNjAwMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjU0Ljg0LjEzOC42MCIsIjU0Ljg0LjY2LjEwOSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.5dV6hjlYzAKxty8UZnU4smCxF4U0aRi7cZuzEOXKQHYJO7xh5aTw3vqMyZ9ThsshYIi7Dxa5ztHIzV_92tFuew");
+                xhr.setRequestHeader('Authorization', $scope.cocApiKey);
             },
             type: 'GET',
             dataType: 'jsonp',
