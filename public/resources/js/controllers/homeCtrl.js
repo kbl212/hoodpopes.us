1d87df6a21bd347812c23c591beb9c7dae0fff3b
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2017-06-18 20:15:01 -0400

    fixed XHR authorization header

--- public/resources/js/controllers/homeCtrl.js
+++ public/resources/js/controllers/homeCtrl.js
@@ -4,15 +4,15 @@ app.controller('homeCtrl', function($scope) {
         $.ajax({
             url: "https://api.clashofclans.com/v1/leagues",
             beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', $scope.cocApiKey);
+                xhr.setRequestHeader('Authorization: Bearer ', $scope.cocApiKey);
             },
             type: 'GET',
             dataType: 'jsonp',
             contentType: 'application/json',
             processdata: false,
             success: function (data) {
+                console.log("SUCCESS");
                 console.log(JSON.stringify(data));
-                console.log('\nhi');
             },
             error: function () {
                 console.log("request error");
