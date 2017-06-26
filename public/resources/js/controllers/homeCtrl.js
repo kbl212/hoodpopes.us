ad2f56c519916b6b070eb6eab2795f1c1c4c14fc
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2017-06-25 20:56:05 -0400

    using Angular  service instead of jQuery ajax

--- public/resources/js/controllers/homeCtrl.js
+++ public/resources/js/controllers/homeCtrl.js
@@ -1,8 +1,7 @@
-app.controller('homeCtrl', function ($scope) {
+app.controller('homeCtrl', function ($scope, $http) {
     $scope.cocApiKey = config.cocApiKey;
-    console.log($scope.cocApiKey);
 
-    function getClanInfo() {
+    /*function getClanInfo() {
         $.ajax({
             url: "https://api.clashofclans.com/v1/leagues",
             beforeSend: function (xhr) {
@@ -22,29 +21,24 @@ app.controller('homeCtrl', function ($scope) {
 
         });
     }
-    getClanInfo();
+    getClanInfo();*/
 
-    function getClanInfoV2() {
-        $.ajax({
-            url: "https://developer.clashofclans.com/v1/leagues",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', $scope.cocApiKey);
+    $scope.getClanInfoV2() {
+        $http({
+            method: 'GET',
+            url: 'https://api.clashofclans.com/v1/locations',
+            headers: {
+                'Authorization': $scope.cocApiKey
             },
-            type: 'GET',
-            dataType: 'jsonp',
-            contentType: 'application/json',
-            processdata: false,
-            success: function (data) {
-                console.log("SUCCESS 2");
-                console.log(JSON.stringify(data));
-            },
-            error: function () {
-                console.log("request error 2...");
-            }
 
+        }).then(function successCallback(response) {
+            console.log('SUCCESS...', response);
+        }, function errorCallback(response) {
+            console.log('NOPE...', response);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
         });
-
     }
-    
-    getClanInfoV2();
+
+    $scope.getClanInfoV2();
 });
