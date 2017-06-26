3f697e999fe44059a34e8d2bcb8110f76ed11a0c
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2017-06-25 20:23:41 -0400

    new request url

--- public/resources/js/controllers/homeCtrl.js
+++ public/resources/js/controllers/homeCtrl.js
@@ -1,6 +1,7 @@
-app.controller('homeCtrl', function($scope) {
+app.controller('homeCtrl', function ($scope) {
     $scope.cocApiKey = config.cocApiKey;
     console.log($scope.cocApiKey);
+
     function getClanInfo() {
         $.ajax({
             url: "https://api.clashofclans.com/v1/leagues",
@@ -22,4 +23,26 @@ app.controller('homeCtrl', function($scope) {
         });
     }
     getClanInfo();
+
+    function getClanInfoV2() {
+        $.ajax({
+            url: "https://developer.clashofclans.com/v1/leagues",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', $scope.cocApiKey);
+            },
+            type: 'GET',
+            dataType: 'jsonp',
+            contentType: 'application/json',
+            processdata: false,
+            success: function (data) {
+                console.log("SUCCESS 2");
+                console.log(JSON.stringify(data));
+            },
+            error: function () {
+                console.log("request error 2...");
+            }
+
+        });
+
+    }
 });
